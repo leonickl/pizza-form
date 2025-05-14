3d0c833c9aa33d1cea5a0a89ea8f89c21a88f8bd
Leo Nickl <leo@leonickl.de>
2025-05-14 13:20:24 +0200

    error page

--- src/Lib/App.php
+++ src/Lib/App.php
@@ -8,11 +8,23 @@ class App
     {
         Session::start();
 
-        $page = Router::route()
-            ->layout('app')
-            ->render();
+        try {
+            $page = Router::route()
+                ->layout('app')
+                ->render();
+        } catch (\Exception $e) {
+            $class = $e::class;
+            $msg = $e->getMessage();
+            $file = $e->getFile();
+            $line = $e->getLine();
+
+            Log::log("Error ($class): $msg in $file on line $line");
+
+            $page = \App\Lib\View::make('error')->layout('app')->render();
+        }
 
         Session::stop();
+
 
         return $page;
     }
