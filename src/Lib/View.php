a51b7015235f0ff7fa20a29be86a63bb30e1d785
Leo Nickl <leo@leonickl.de>
2025-05-26 19:08:24 +0200

    format

--- src/Lib/View.php
+++ src/Lib/View.php
@@ -2,35 +2,36 @@
 
 namespace App\Lib;
 
-class View {
-    private const string PATH = __DIR__ . '/../../views';
+class View
+{
+	private const string PATH = __DIR__ . '/../../views';
 
 	private function __construct(protected string $view, protected array $params) {}
 
- 	public static function make(string $view, array $params = []): self
+	public static function make(string $view, array $params = []): self
 	{
-  		return new self($view, $params);
-    }
+		return new self($view, $params);
+	}
 
- 	public function render(): string
+	public function render(): string
 	{
-  		$path = self::PATH . '/' . $this->view . '.php';
+		$path = self::PATH . '/' . $this->view . '.php';
 
-    	if(!file_exists($path)) {
-     		throw new \App\Exceptions\ViewNotFoundException($path);
-        }
-        
-        extract($this->params);
-        
-  		ob_start();
-    	include $path;
-     	return ob_get_clean();
-    }
+		if (!file_exists($path)) {
+			throw new \App\Exceptions\ViewNotFoundException($path);
+		}
+
+		extract($this->params);
+
+		ob_start();
+		include $path;
+		return ob_get_clean();
+	}
 
 	public function layout(string $view)
 	{
-       	return self::make($view, [
-            'slot' => $this->render(),
+		return self::make($view, [
+			'slot' => $this->render(),
 			...$this->params,
 		]);
 	}
