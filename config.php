21e9e2f363d2bc984e825c2c507f9509e9c34257
Leo Nickl <leo@leonickl.de>
2025-07-31 15:57:04 +0200

    move routes to config

--- config.php
+++ config.php
@@ -2,4 +2,23 @@
 
 return [
     'domain' => 'pizza.leonickl.de',
+
+    'routes' => [
+        '/' => [
+            'GET' => [\App\Controllers\OrderController::class, 'index'],
+            'POST' => [\App\Controllers\OrderController::class, 'action'],
+        ],
+        '/90d13090-fa3b-480f-a6d2-3e06fec20954' => [
+            'GET' => [\App\Controllers\AdminController::class, 'index'],
+        ],
+        '/90d13090-fa3b-480f-a6d2-3e06fec20954/analysis' => [
+            'GET' => [\App\Controllers\AdminController::class, 'analysis'],
+        ],
+        '/90d13090-fa3b-480f-a6d2-3e06fec20954/delete' => [
+            'POST' => [\App\Controllers\AdminController::class, 'destroy'],
+        ],
+        '/90d13090-fa3b-480f-a6d2-3e06fec20954/toggle-paid' => [
+            'POST' => [\App\Controllers\AdminController::class, 'togglePaid'],
+        ],
+    ],
 ];
--- src/Lib/Router.php
+++ src/Lib/Router.php
@@ -19,24 +19,7 @@ class Router
 
     private static function find(string $path, string $method)
     {
-        $routes = [
-            '/' => [
-                'GET' => [\App\Controllers\OrderController::class, 'index'],
-                'POST' => [\App\Controllers\OrderController::class, 'action'],
-            ],
-            '/90d13090-fa3b-480f-a6d2-3e06fec20954' => [
-                'GET' => [\App\Controllers\AdminController::class, 'index'],
-            ],
-            '/90d13090-fa3b-480f-a6d2-3e06fec20954/analysis' => [
-                'GET' => [\App\Controllers\AdminController::class, 'analysis'],
-            ],
-            '/90d13090-fa3b-480f-a6d2-3e06fec20954/delete' => [
-                'POST' => [\App\Controllers\AdminController::class, 'destroy'],
-            ],
-            '/90d13090-fa3b-480f-a6d2-3e06fec20954/toggle-paid' => [
-                'POST' => [\App\Controllers\AdminController::class, 'togglePaid'],
-            ],
-        ];
+        $routes = config('routes');
 
         if (!array_key_exists($path, $routes)) {
             return [\App\Controllers\ErrorController::class, 'notFound', [$path]];
