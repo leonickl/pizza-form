0d99d414b63ad7950662cbb09bb2f0650bb728b3
Leo Nickl <leo@leonickl.de>
2026-05-01 21:34:35 +0200

    conditionally show nav items

--- views/admin.nav.plate.php
+++ views/admin.nav.plate.php
@@ -1,11 +1,15 @@
 <div class="nav row mb">
-    <a href="/admin" class="btn">Bestellungen</a>
-    <a href="/admin/trash" class="btn">Papierkorb</a>
-    <a href="/admin/analysis" class="btn">Analyse</a>
-    <a href="/admin/toggle-accessibility?__method=post" class="btn"
-        style="background-color: {{ perma('accessible', false) ? 'lightgreen' : 'red' }}">
-            Zugang umschalten
-    </a>
+    {{ if: url() === '/admin' }}
+        <a href="/admin/trash" class="btn">Papierkorb</a>
+        <a href="/admin/analysis" class="btn">Analyse</a>
+        <a href="/admin/toggle-accessibility?__method=post" class="btn"
+            style="background-color: {{ perma('accessible', false) ? 'lightgreen' : 'red' }}">
+                Zugang umschalten
+        </a>
+    {{ else: }}
+        <a href="/admin" class="btn">Bestellungen</a>
+    {{ if; }}
+
     <a class="btn secondary" href="/">Formular</a>
     <a class="btn warn" href="/logout">Logout</a>
 </div>
