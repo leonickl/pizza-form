ce716910d3d90d0272d439fa5931cb9995815a5d
leonickl <77433904+leonickl@users.noreply.github.com>
2026-05-14 12:56:07 +0000

    Apply automatic changes

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@ function head(string $heading): string
 {
     $nav = new Nav;
 
-    return "<div class=\"row wrap between\">
-        <h1>".e($heading)."</h1>
+    return '<div class="row wrap between">
+        <h1>'.e($heading)."</h1>
         <div>
             <div class=\"row wrap mb\">$nav</div>
         </div>
