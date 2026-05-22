cff5f01a6e33ab832b5da380143da10296f394e7
Leo Nickl <leo@leonickl.de>
2026-05-22 17:00:14 +0200

    create user from cli

---
+++ commands.php
@@ -0,0 +1,27 @@
+<?php
+
+use PXP\Console\Command;
+use App\Models\User;
+
+Command::new('create:user', function (?string $name, ?string $email, ?string $password, string $role = '0') {
+    if ($name === null) {
+        exit("Please enter a name\n");
+    }
+
+    if ($email === null) {
+        exit("Please enter an email address\n");
+    }
+
+    if ($password === null) {
+        exit("Please enter a password\n");
+    }
+
+    $user = User::create(
+        name: $name,
+        username: $email,
+        password_hash: password_hash($password, PASSWORD_DEFAULT),
+        role: (int)$role,
+    );
+
+    echo "created user with id $user->id\n";
+});
